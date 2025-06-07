2e3b1f638b2e902769c5d8c0be6cde12ec3ed9a1
rubens junio <rubens.marques@hotmart.com>
2025-06-07 09:52:29 -0300

    chore: Update build script in package.json and add tsup configuration file

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/server.ts'],
+  format: ['cjs'],
+  dts: true,
+  clean: true,
+  sourcemap: true,
+  outDir: 'build',
+  noExternal: ['@/*'],
+  esbuildOptions(options) {
+    options.resolveExtensions = ['.ts', '.js']
+    options.alias = {
+      '@': './src',
+    }
+  },
+})
